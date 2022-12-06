e0f3da1ecffcea064424aeb71e817024b51f0ee0
opaz99 <opaz@grupoplatino.hn>
2022-12-05 21:27:41 -0600

    Fix route in app

--- api/index.php
+++ api/index.php
@@ -5,5 +5,5 @@ require "../functions.php";
 
 $functions = new Functions($mysqli);
 $module = isset($_GET['module']) ? $_GET['module'] : 'home';
-require "app/index.php";
+require "../app/index.php";
 ?>
